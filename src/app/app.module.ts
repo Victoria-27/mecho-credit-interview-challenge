05e2c44e825af23d03119182dc56c77fdfa91984
Victoria Abioye <victoriousvikky003@gmail.com>
2023-08-16 09:16:23 +0100

    Architectural setup completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { CustomerComponent } from './customer/customer.component';
+import { RequestComponent } from './request/request.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomerComponent,
+    RequestComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]

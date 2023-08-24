d1460e2a9adc5fc2626e1432487b435ce0ce81ba
Victoria Abioye <victoriousvikky003@gmail.com>
2023-08-24 22:15:41 +0100

    complete test for page header component

--- src/app/components/page-header/page-header.component.spec.ts
+++ src/app/components/page-header/page-header.component.spec.ts
@@ -1,43 +1,90 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { PageHeaderComponent } from './page-header.component';
 import { FormsModule } from '@angular/forms';
-import { Component, OnInit } from '@angular/core';
-import { Observable, of } from 'rxjs';
-import { map } from 'rxjs/operators';
-import { Customer } from 'src/app/models/customer.model';
+import { of } from 'rxjs';
+import { PageHeaderComponent } from './page-header.component';
 import { UsersService } from 'src/app/services/users.service';
-import { HttpClientTestingModule } from '@angular/common/http/testing'; 
+import { Customer } from 'src/app/models/customer.model';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('PageHeaderComponent', () => {
   let component: PageHeaderComponent;
   let fixture: ComponentFixture<PageHeaderComponent>;
+  let usersService: UsersService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [PageHeaderComponent],
-      imports: [HttpClientTestingModule],
-      providers: [UsersService]
+      imports: [FormsModule, HttpClientTestingModule],
+      providers: [UsersService],
     });
 
     fixture = TestBed.createComponent(PageHeaderComponent);
     component = fixture.componentInstance;
-    spyOn(sessionStorage, 'getItem').and.returnValue(
-      JSON.stringify({ userEmail: 'test@example.com', /* ... other properties */ })
-    ); // Mock getItem to provide valid JSON data
+    usersService = TestBed.inject(UsersService);
 
-    fixture = TestBed.createComponent(PageHeaderComponent);
+    spyOn(sessionStorage, 'getItem').and.returnValue(
+      JSON.stringify({ userEmail: 'test@example.com', balance: 85000})
+    );
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 
-  // ... other test cases
+  it('should initialize with an empty selected customer', () => {
+    expect(component.selectedCustomer).toBe('');
+  });
+
+  it('should initialize with no customers', () => {
+    expect(component.customers).toBeUndefined();
+  });
+
+  it('should populate customerEmailOptions$ on initialization', () => {
+    const customers: Customer[] = [
+      { userEmail: 'user1@example.com' }
+    
+    ];
+  
+    spyOn(usersService, 'getCustomers').and.returnValue(of(customers));
+  
+    component.ngOnInit();
+  
+    component.customerEmailOptions$.subscribe((options) => {
+      const customerEmails = customers.map((customer) => customer.userEmail);
+  
+      const optionLabels = options.map((option) => option.label);
+      const optionValues = options.map((option) => option.value);
+  
+      optionLabels.forEach((label) => {
+        expect(customerEmails).toContain(label);
+      });
+  
+      optionValues.forEach((value) => {
+        expect(customerEmails).toContain(value);
+      });
+    });
+  });
+  
+  
+  it('should set the selected customer and call setSelectedCustomer method', () => {
+    const customers: Customer[] = [
+      { userEmail: 'user1@example.com' }
+      
+    ];
+  
+    spyOn(usersService, 'getCustomers').and.returnValue(of(customers));
+  
+    component.ngOnInit();
+  
+    component.customers = customers;
+  
+    component.selectedCustomer = 'user1@example.com'; 
+  
+    const setSelectedCustomerSpy = spyOn(usersService, 'setSelectedCustomer'); 
+  
+    component.setSelectedCustomer();
+  
+    expect(setSelectedCustomerSpy).toHaveBeenCalledWith(customers[0]); 
+  });
+  
 });
-
-
-
-
-
-
